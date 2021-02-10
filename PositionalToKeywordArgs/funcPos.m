c007cbe1f37f75eb6e98fe876db81fe67055f1d0
Pranay S. Yadav <mail@pranaysy.com>
2021-02-10 12:10:04 +0530

    Updated with comments and descriptions

--- PositionalToKeywordArgs/funcPos.m
+++ PositionalToKeywordArgs/funcPos.m
@@ -1,22 +1,28 @@
 function [ output ] = funcPos( inputVector, varargin )
 %FUNCPOS Compute either the mean or the median of an input numeric vector
-%   Detailed explanation goes here
+%   Demo for parsing inputs to function provided as positional arguments
     
+    % Create an InputParser object
     p = inputParser;
     
-    defaultEstimate = 'mean';
-    validEstimates = {'mean', 'median'};
-    checkEstimates = @(x) any(validatestring(x, validEstimates));
+    % Specify possible inputs
+    defaultEstimate = 'mean'; % Default input to prefer
+    validEstimates = {'mean', 'median'}; % Possible choices for input
+    checkEstimates = @(x) any(validatestring(x, validEstimates)); % Anonymous function to check/validate inputs from list of possible choices
     
-    addRequired(p, 'inputvector', @isnumeric);
-    addOptional(p, 'estimate', defaultEstimate, checkEstimates);
+    % Add inputs to the InputParser
+    addRequired(p, 'inputvector', @isnumeric); % This input is required and can't not be provided
+    addOptional(p, 'estimate', defaultEstimate, checkEstimates); % This input may or may not be provided since there exists a default choice
     
+    % Throw a fit if the inputs do not match the provided parsing scheme
     p.KeepUnmatched = false;
     
+    % Parse the inputs based on the specified scheme in the InputParser object
     parse(p, inputVector, varargin{:});
     
-    functionToExec = str2func(p.Results.estimate);
-    output = functionToExec(inputVector);
+    % Do what the function is supposed to do
+    functionToExec = str2func(p.Results.estimate); % Convert the input string to a function
+    output = functionToExec(inputVector); % Execute the function and return output
 
 end
 
